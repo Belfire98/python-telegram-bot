484d320b6b273a2543ff2d4a88fd0197f17e066a
Belfire98 <93988830+Belfire98@users.noreply.github.com>
2024-03-19 01:23:42 +0100

    improve

--- telegram/_files/document.py
+++ telegram/_files/document.py
@@ -1,28 +1,10 @@
 #!/usr/bin/env python
-#
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2024
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
-"""This module contains an object that represents a Telegram Document."""
-from typing import Optional
+
+from typing import Dict, Optional
 
 from telegram._files._basethumbedmedium import _BaseThumbedMedium
 from telegram._files.photosize import PhotoSize
 from telegram._utils.types import JSONDict
-
 
 class Document(_BaseThumbedMedium):
     """This object represents a general file
@@ -31,30 +13,27 @@ class Document(_BaseThumbedMedium):
     Objects of this class are comparable in terms of equality. Two objects of this class are
     considered equal, if their :attr:`file_unique_id` is equal.
 
-    .. versionchanged:: 20.5
-      |removed_thumb_note|
-
     Args:
-        file_id (:obj:`str`): Identifier for this file, which can be used to download
+        file_id (str): Identifier for this file, which can be used to download
             or reuse the file.
-        file_unique_id (:obj:`str`): Unique identifier for this file, which is supposed to be
+        file_unique_id (str): Unique identifier for this file, which is supposed to be
             the same over time and for different bots. Can't be used to download or reuse the file.
-        file_name (:obj:`str`, optional): Original filename as defined by sender.
-        mime_type (:obj:`str`, optional): MIME type of the file as defined by sender.
-        file_size (:obj:`int`, optional): File size in bytes.
-        thumbnail (:class:`telegram.PhotoSize`, optional): Document thumbnail as defined by sender.
+        file_name (Optional[str]): Original filename as defined by sender.
+        mime_type (Optional[str]): MIME type of the file as defined by sender.
+        file_size (Optional[int]): File size in bytes.
+        thumbnail (Optional[PhotoSize]): Document thumbnail as defined by sender.
 
             .. versionadded:: 20.2
 
     Attributes:
-        file_id (:obj:`str`): Identifier for this file, which can be used to download
+        file_id (str): Identifier for this file, which can be used to download
             or reuse the file.
-        file_unique_id (:obj:`str`): Unique identifier for this file, which is supposed to be
+        file_unique_id (str): Unique identifier for this file, which is supposed to be
             the same over time and for different bots. Can't be used to download or reuse the file.
-        file_name (:obj:`str`): Optional. Original filename as defined by sender.
-        mime_type (:obj:`str`): Optional. MIME type of the file as defined by sender.
-        file_size (:obj:`int`): Optional. File size in bytes.
-        thumbnail (:class:`telegram.PhotoSize`): Optional. Document thumbnail as defined by sender.
+        file_name (Optional[str]): Optional. Original filename as defined by sender.
+        mime_type (Optional[str]): Optional. MIME type of the file as defined by sender.
+        file_size (Optional[int]): Optional. File size in bytes.
+        thumbnail (Optional[PhotoSize]): Optional. Document thumbnail as defined by sender.
 
             .. versionadded:: 20.2
 
@@ -80,7 +59,53 @@ class Document(_BaseThumbedMedium):
             thumbnail=thumbnail,
             api_kwargs=api_kwargs,
         )
-        with self._unfrozen():
-            # Optional
-            self.mime_type: Optional[str] = mime_type
-            self.file_name: Optional[str] = file_name
+        self.file_name: Optional[str] = file_name
+        self.mime_type: Optional[str] = mime_type
+
+    def __repr__(self):
+        """Return a string representation of the object."""
+        attrs = [
+            f"{k}={repr(v)}" for k, v in self.__dict__.items() if not k.startswith("_")
+        ]
+        return f"Document({', '.join(attrs)})"
+
+    @classmethod
+    def from_dict(cls, data: Dict):
+        """Create a new Document object from a dictionary.
+
+        Args:
+            data (Dict): A dictionary representing the Document object.
+
+        Returns:
+            Document: A new Document object.
+
+        """
+        return cls(
+            file_id=data["file_id"],
+            file_unique_id=data["file_unique_id"],
+            file_name=data.get("file_name"),
+            mime_type=data.get("mime_type"),
+            file_size=data.get("file_size"),
+            thumbnail=PhotoSize.from_dict(data.get("thumbnail")) if data.get("thumbnail") else None,
+            api_kwargs=data.get("api_kwargs"),
+        )
+
+    def to_dict(self):
+        """Create a dictionary representation of the object.
+
+        Returns:
+            Dict: A dictionary representing the Document object.
+
+        """
+        data = {
+            "file_id": self.file_id,
+            "file_unique_id": self.file_unique_id,
+            "file_size": self.file_size,
+            "thumbnail": self.thumbnail.to_dict() if self.thumbnail else None,
+            "api_kwargs": self.api_kwargs,
+        }
+        if self.file_name is not None:
+            data["file_name"] = self.file_name
+        if self.mime_type is not None:
+            data["mime_type"] = self.mime_type
+        return data
