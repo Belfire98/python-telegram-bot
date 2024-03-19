5ba4e97bcd9053a97083f9fb0ac265ebeaccb10e
Belfire98 <93988830+Belfire98@users.noreply.github.com>
2024-03-19 01:23:48 +0100

    improve

--- telegram/ext/_handlers/prefixhandler.py
+++ telegram/ext/_handlers/prefixhandler.py
@@ -85,11 +85,11 @@ class PrefixHandler(BaseHandler[Update, CCT]):
         * Updating the prefixes and commands this handler listens to is no longer possible.
 
     Args:
-        prefix (:obj:`str` | Collection[:obj:`str`]):
+        prefix (Union[str, Sequence[str]]):
             The prefix(es) that will precede :paramref:`command`.
-        command (:obj:`str` | Collection[:obj:`str`]):
+        command (Union[str, Sequence[str]]):
             The command or list of commands this handler should listen for. Case-insensitive.
-        callback (:term:`coroutine function`): The callback function for this handler. Will be
+        callback (HandlerCallback[Update, CCT, RT]): The callback function for this handler. Will be
             called when :meth:`check_update` has determined that an update should be processed by
             this handler. Callback signature::
 
@@ -97,88 +97,7 @@ class PrefixHandler(BaseHandler[Update, CCT]):
 
             The return value of the callback is usually ignored except for the special case of
             :class:`telegram.ext.ConversationHandler`.
-        filters (:class:`telegram.ext.filters.BaseFilter`, optional): A filter inheriting from
+        filters (Optional[filters_module.BaseFilter] = None): A filter inheriting from
             :class:`telegram.ext.filters.BaseFilter`. Standard filters can be found in
             :mod:`telegram.ext.filters`. Filters can be combined using bitwise
-            operators (``&`` for :keyword:`and`, ``|`` for :keyword:`or`, ``~`` for :keyword:`not`)
-        block (:obj:`bool`, optional): Determines whether the return value of the callback should
-            be awaited before processing the next handler in
-            :meth:`telegram.ext.Application.process_update`. Defaults to :obj:`True`.
-
-            .. seealso:: :wiki:`Concurrency`
-
-    Attributes:
-        commands (FrozenSet[:obj:`str`]): The commands that this handler will listen for, i.e. the
-            combinations of :paramref:`prefix` and :paramref:`command`.
-        callback (:term:`coroutine function`): The callback function for this handler.
-        filters (:class:`telegram.ext.filters.BaseFilter`): Optional. Only allow updates with these
-            Filters.
-        block (:obj:`bool`): Determines whether the return value of the callback should be
-            awaited before processing the next handler in
-            :meth:`telegram.ext.Application.process_update`.
-
-    """
-
-    # 'prefix' is a class property, & 'command' is included in the superclass, so they're left out.
-    __slots__ = ("commands", "filters")
-
-    def __init__(
-        self,
-        prefix: SCT[str],
-        command: SCT[str],
-        callback: HandlerCallback[Update, CCT, RT],
-        filters: Optional[filters_module.BaseFilter] = None,
-        block: DVType[bool] = DEFAULT_TRUE,
-    ):
-        super().__init__(callback=callback, block=block)
-
-        prefixes = {prefix.lower()} if isinstance(prefix, str) else {x.lower() for x in prefix}
-
-        commands = {command.lower()} if isinstance(command, str) else {x.lower() for x in command}
-
-        self.commands: FrozenSet[str] = frozenset(
-            p + c for p, c in itertools.product(prefixes, commands)
-        )
-        self.filters: filters_module.BaseFilter = (
-            filters if filters is not None else filters_module.UpdateType.MESSAGES
-        )
-
-    def check_update(
-        self, update: object
-    ) -> Optional[Union[bool, Tuple[List[str], Optional[Union[bool, Dict[Any, Any]]]]]]:
-        """Determines whether an update should be passed to this handler's :attr:`callback`.
-
-        Args:
-            update (:class:`telegram.Update` | :obj:`object`): Incoming update.
-
-        Returns:
-            :obj:`list`: The list of args for the handler.
-
-        """
-        if isinstance(update, Update) and update.effective_message:
-            message = update.effective_message
-
-            if message.text:
-                text_list = message.text.split()
-                if text_list[0].lower() not in self.commands:
-                    return None
-                filter_result = self.filters.check_update(update)
-                if filter_result:
-                    return text_list[1:], filter_result
-                return False
-        return None
-
-    def collect_additional_context(
-        self,
-        context: CCT,
-        update: Update,
-        application: "Application[Any, CCT, Any, Any, Any, Any]",
-        check_result: Optional[Union[bool, Tuple[List[str], Optional[bool]]]],
-    ) -> None:
-        """Add text after the command to :attr:`CallbackContext.args` as list, split on single
-        whitespaces and add output of data filters to :attr:`CallbackContext` as well.
-        """
-        if isinstance(check_result, tuple):
-            context.args = check_result[0]
-            if isinstance(check_result[1], dict):
-                context.update(check_result[1])
+            operators (``&`` for :keyword:`and`, ``|`` for :keyword:`or`, ``
