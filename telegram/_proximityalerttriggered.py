4e6f31450b91f3f90c0a3905e63f4a9c0e395764
Belfire98 <93988830+Belfire98@users.noreply.github.com>
2024-03-19 01:23:43 +0100

    improve

--- telegram/_proximityalerttriggered.py
+++ telegram/_proximityalerttriggered.py
@@ -1,31 +1,12 @@
 #!/usr/bin/env python
-#
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2024
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
-"""This module contains an object that represents a Telegram Proximity Alert."""
-from typing import TYPE_CHECKING, Optional
 
+from typing import TYPE_CHECKING, Optional
 from telegram._telegramobject import TelegramObject
 from telegram._user import User
 from telegram._utils.types import JSONDict
 
 if TYPE_CHECKING:
     from telegram import Bot
-
 
 class ProximityAlertTriggered(TelegramObject):
     """
@@ -36,17 +17,16 @@ class ProximityAlertTriggered(TelegramObject):
     considered equal, if their :attr:`traveler`, :attr:`watcher` and :attr:`distance` are equal.
 
     Args:
-        traveler (:class:`telegram.User`): User that triggered the alert
-        watcher (:class:`telegram.User`): User that set the alert
-        distance (:obj:`int`): The distance between the users
+        traveler (User): User that triggered the alert
+        watcher (User): User that set the alert
+        distance (int): The distance between the users
 
     Attributes:
-        traveler (:class:`telegram.User`): User that triggered the alert
-        watcher (:class:`telegram.User`): User that set the alert
-        distance (:obj:`int`): The distance between the users
+        traveler (User): User that triggered the alert
+        watcher (User): User that set the alert
+        distance (int): The distance between the users
 
     """
-
     __slots__ = ("distance", "traveler", "watcher")
 
     def __init__(
@@ -68,7 +48,17 @@ class ProximityAlertTriggered(TelegramObject):
 
     @classmethod
     def de_json(cls, data: Optional[JSONDict], bot: "Bot") -> Optional["ProximityAlertTriggered"]:
-        """See :meth:`telegram.TelegramObject.de_json`."""
+        """
+        Creates an instance of ProximityAlertTriggered from its JSON representation.
+
+        Args:
+            data (Optional[JSONDict]): The JSON representation of the ProximityAlertTriggered object.
+            bot (Bot): The Bot object associated with this ProximityAlertTriggered object.
+
+        Returns:
+            Optional[ProximityAlertTriggered]: An instance of ProximityAlertTriggered, or None if the
+            data is None or invalid.
+        """
         data = cls._parse_data(data)
 
         if not data:
