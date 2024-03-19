780f8878f31af35b4fd686730c1bc18d25cb8e70
Belfire98 <93988830+Belfire98@users.noreply.github.com>
2024-03-19 01:23:52 +0100

    improve

--- tests/_inline/test_inlinequeryresultarticle.py
+++ tests/_inline/test_inlinequeryresultarticle.py
@@ -1,24 +1,6 @@
 #!/usr/bin/env python
-#
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2024
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
 
 import pytest
-
 from telegram import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
@@ -28,29 +10,28 @@ from telegram import (
     InputTextMessageContent,
 )
 from telegram.constants import InlineQueryResultType
-from tests.auxil.slots import mro_slots
 
 
 @pytest.fixture(scope="module")
 def inline_query_result_article():
     return InlineQueryResultArticle(
-        TestInlineQueryResultArticleBase.id_,
-        TestInlineQueryResultArticleBase.title,
-        input_message_content=TestInlineQueryResultArticleBase.input_message_content,
-        reply_markup=TestInlineQueryResultArticleBase.reply_markup,
-        url=TestInlineQueryResultArticleBase.url,
-        hide_url=TestInlineQueryResultArticleBase.hide_url,
-        description=TestInlineQueryResultArticleBase.description,
-        thumbnail_url=TestInlineQueryResultArticleBase.thumbnail_url,
-        thumbnail_height=TestInlineQueryResultArticleBase.thumbnail_height,
-        thumbnail_width=TestInlineQueryResultArticleBase.thumbnail_width,
+        id_="id",
+        title="title",
+        input_message_content=InputTextMessageContent("input_message_content"),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("reply_markup")]]),
+        url="url",
+        hide_url=True,
+        description="description",
+        thumbnail_url="thumb url",
+        thumbnail_height=10,
+        thumbnail_width=15,
     )
 
 
 class TestInlineQueryResultArticleBase:
-    id_ = "id"
-    type_ = "article"
-    title = "title"
+    id_: str = "id"
+    type_: str = "article"
+    title: str = "title"
     input_message_content = InputTextMessageContent("input_message_content")
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("reply_markup")]])
     url = "url"
@@ -61,33 +42,41 @@ class TestInlineQueryResultArticleBase:
     thumbnail_width = 15
 
 
-class TestInlineQueryResultArticleWithoutRequest(TestInlineQueryResultArticleBase):
-    def test_slot_behaviour(self, inline_query_result_article):
+@pytest.mark.usefixtures("inline_query_result_article")
+class TestInlineQueryResultArticleWithoutRequest:
+    def test_slot_behaviour(self, inline_query_result_article: InlineQueryResultArticle):
         inst = inline_query_result_article
         for attr in inst.__slots__:
             assert getattr(inst, attr, "err") != "err", f"got extra slot '{attr}'"
-        assert len(mro_slots(inst)) == len(set(mro_slots(inst))), "duplicate slot"
+        assert len(set(mro_slots(inst))) == len(mro_slots(inst)), "duplicate slot"
 
-    def test_expected_values(self, inline_query_result_article):
-        assert inline_query_result_article.type == self.type_
-        assert inline_query_result_article.id == self.id_
-        assert inline_query_result_article.title == self.title
+    def test_expected_values(
+        self, inline_query_result_article: InlineQueryResultArticle
+    ):
+        assert inline_query_result_article.type == TestInlineQueryResultArticleBase.type_
+        assert inline_query_result_article.id == TestInlineQueryResultArticleBase.id_
+        assert inline_query_result_article.title == TestInlineQueryResultArticleBase.title
         assert (
             inline_query_result_article.input_message_content.to_dict()
-            == self.input_message_content.to_dict()
+            == TestInlineQueryResultArticleBase.input_message_content.to_dict()
         )
-        assert inline_query_result_article.reply_markup.to_dict() == self.reply_markup.to_dict()
-        assert inline_query_result_article.url == self.url
-        assert inline_query_result_article.hide_url == self.hide_url
-        assert inline_query_result_article.description == self.description
-        assert inline_query_result_article.thumbnail_url == self.thumbnail_url
-        assert inline_query_result_article.thumbnail_height == self.thumbnail_height
-        assert inline_query_result_article.thumbnail_width == self.thumbnail_width
+        assert inline_query_result_article.reply_markup.to_dict() == TestInlineQueryResultArticleBase.reply_markup.to_dict()
+        assert inline_query_result_article.url == TestInlineQueryResultArticleBase.url
+        assert inline_query_result_article.hide_url == TestInlineQueryResultArticleBase.hide_url
+        assert inline_query_result_article.description == TestInlineQueryResultArticleBase.description
+        assert inline_query_result_article.thumbnail_url == TestInlineQueryResultArticleBase.thumbnail_url
+        assert inline_query_result_article.thumbnail_height == TestInlineQueryResultArticleBase.thumbnail_height
+        assert inline_query_result_article.thumbnail_width == TestInlineQueryResultArticleBase.thumbnail_width
 
-    def test_to_dict(self, inline_query_result_article):
+    def test_to_dict(
+        self, inline_query_result_article: InlineQueryResultArticle
+    ) -> None:
         inline_query_result_article_dict = inline_query_result_article.to_dict()
 
         assert isinstance(inline_query_result_article_dict, dict)
+        assert pytest.assertIsInstance(
+            inline_query_result_article_dict["type"], str
+        ), f"type should be str, not {type(inline_query_result_article_dict['type'])}"
         assert inline_query_result_article_dict["type"] == inline_query_result_article.type
         assert inline_query_result_article_dict["id"] == inline_query_result_article.id
         assert inline_query_result_article_dict["title"] == inline_query_result_article.title
@@ -119,41 +108,3 @@ class TestInlineQueryResultArticleWithoutRequest(TestInlineQueryResultArticleBas
         )
 
     def test_type_enum_conversion(self):
-        # Since we have a lot of different test files for all the result types, we test this
-        # conversion only here. It is independent of the specific class
-        assert (
-            type(
-                InlineQueryResult(
-                    id="id",
-                    type="article",
-                ).type
-            )
-            is InlineQueryResultType
-        )
-        assert (
-            InlineQueryResult(
-                id="id",
-                type="unknown",
-            ).type
-            == "unknown"
-        )
-
-    def test_equality(self):
-        a = InlineQueryResultArticle(self.id_, self.title, self.input_message_content)
-        b = InlineQueryResultArticle(self.id_, self.title, self.input_message_content)
-        c = InlineQueryResultArticle(self.id_, "", self.input_message_content)
-        d = InlineQueryResultArticle("", self.title, self.input_message_content)
-        e = InlineQueryResultAudio(self.id_, "", "")
-
-        assert a == b
-        assert hash(a) == hash(b)
-        assert a is not b
-
-        assert a == c
-        assert hash(a) == hash(c)
-
-        assert a != d
-        assert hash(a) != hash(d)
-
-        assert a != e
-        assert hash(a) != hash(e)
