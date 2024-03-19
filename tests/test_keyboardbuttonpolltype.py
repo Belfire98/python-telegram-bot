f10b873e060f1edaa936ff489a990008e15e73ae
Belfire98 <93988830+Belfire98@users.noreply.github.com>
2024-03-19 01:23:53 +0100

    improve

--- tests/test_keyboardbuttonpolltype.py
+++ tests/test_keyboardbuttonpolltype.py
@@ -1,69 +1,48 @@
 #!/usr/bin/env python
-#
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2024
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
+
 import pytest
-
-from telegram import KeyboardButtonPollType, Poll
-from telegram.constants import PollType
-from tests.auxil.slots import mro_slots
+from telegram import KeyboardButtonPollType, Poll, PollType
+from telegram.constants import PollType as PollTypeConstant
 
 
-@pytest.fixture(scope="module")
-def keyboard_button_poll_type():
-    return KeyboardButtonPollType(TestKeyboardButtonPollTypeBase.type)
+class TestKeyboardButtonPollType:
+    """Tests for the KeyboardButtonPollType class."""
 
+    @pytest.fixture(scope="module")
+    def keyboard_button_poll_type(self):
+        return KeyboardButtonPollType(self.type)
 
-class TestKeyboardButtonPollTypeBase:
     type = Poll.QUIZ
 
-
-class TestKeyboardButtonPollTypeWithoutRequest(TestKeyboardButtonPollTypeBase):
+    @pytest.mark.usefixtures("keyboard_button_poll_type")
     def test_slot_behaviour(self, keyboard_button_poll_type):
         inst = keyboard_button_poll_type
         for attr in inst.__slots__:
             assert getattr(inst, attr, "err") != "err", f"got extra slot '{attr}'"
         assert len(mro_slots(inst)) == len(set(mro_slots(inst))), "duplicate slot"
 
+    @pytest.mark.usefixtures("keyboard_button_poll_type")
     def test_to_dict(self, keyboard_button_poll_type):
         keyboard_button_poll_type_dict = keyboard_button_poll_type.to_dict()
         assert isinstance(keyboard_button_poll_type_dict, dict)
         assert keyboard_button_poll_type_dict["type"] == self.type
 
-    def test_type_enum_conversion(self):
-        assert (
-            type(
-                KeyboardButtonPollType(
-                    type="quiz",
-                ).type
-            )
-            is PollType
-        )
-        assert (
-            KeyboardButtonPollType(
-                type="unknown",
-            ).type
-            == "unknown"
-        )
+    @pytest.mark.parametrize(
+        "poll_type_str, expected_result",
+        [
+            ("quiz", PollTypeConstant.QUIZ),
+            ("regular", PollTypeConstant.REGULAR),
+            ("unknown", "unknown"),
+        ],
+    )
+    def test_type_enum_conversion(self, poll_type_str, expected_result):
+        result = KeyboardButtonPollType(type=poll_type_str).type
+        assert result == expected_result
 
     def test_equality(self):
-        a = KeyboardButtonPollType(Poll.QUIZ)
-        b = KeyboardButtonPollType(Poll.QUIZ)
-        c = KeyboardButtonPollType(Poll.REGULAR)
+        a = KeyboardButtonPollType(PollTypeConstant.QUIZ)
+        b = KeyboardButtonPollType(PollTypeConstant.QUIZ)
+        c = KeyboardButtonPollType(PollTypeConstant.REGULAR)
 
         assert a == b
         assert hash(a) == hash(b)
