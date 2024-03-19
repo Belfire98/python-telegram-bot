27dc694988d4bac308ccc8d7124b754be0da3efa
Belfire98 <93988830+Belfire98@users.noreply.github.com>
2024-03-19 01:23:43 +0100

    improve

--- telegram/_utils/logging.py
+++ telegram/_utils/logging.py
@@ -1,50 +1,61 @@
 #!/usr/bin/env python
-#
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2024
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
-"""This module contains helper functions related to logging.
 
-Warning:
-    Contents of this module are intended to be used internally by the library and *not* by the
-    user. Changes to this module are not considered breaking changes and may not be documented in
-    the changelog.
-"""
 import logging
+import os
+import sys
 from typing import Optional
 
+try:
+    import fcntl
+except ImportError:
+    fcntl = None
 
-def get_logger(file_name: str, class_name: Optional[str] = None) -> logging.Logger:
-    """Returns a logger with an appropriate name.
-    Use as follows::
 
-        logger = get_logger(__name__)
-
-    If for example `__name__` is `telegram.ext._updater`, the logger will be named
-    `telegram.ext.Updater`. If `class_name` is passed, this will result in
-    `telegram.ext.<class_name>`. Useful e.g. for CamelCase class names.
+def get_logger(*args, **kwargs) -> logging.Logger:
+    """
+    Returns a logger object with an appropriate name.
 
     If the file name points to a utils module, the logger name will simply be `telegram(.ext)`.
+
+    Args:
+        *args: Variable length argument list.
+        **kwargs: Arbitrary keyword arguments.
 
     Returns:
         :class:`logging.Logger`: The logger.
     """
+    class_name = kwargs.get("class_name")
+    file_name = args[0]
+
     parts = file_name.split("_")
     if parts[1].startswith("utils") and class_name is None:
         name = parts[0].rstrip(".")
     else:
         name = f"{parts[0]}{class_name or parts[1].capitalize()}"
-    return logging.getLogger(name)
+
+    logger = logging.getLogger(name)
+
+    # Set up logging to a file
+    log_file = kwargs.get("log_file")
+    if log_file:
+        log_dir = os.path.dirname(log_file)
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir)
+
+        if not logger.handlers:
+            logger.setLevel(logging.DEBUG)
+
+            formatter = logging.Formatter(
+                fmt="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s",
+                datefmt="%Y-%m-%d %H:%M:%S",
+            )
+
+            file_handler = logging.FileHandler(log_file)
+            file_handler.setFormatter(formatter)
+            logger.addHandler(file_handler)
+
+            if fcntl:
+                fd = file_handler.stream.fileno()
+                fcntl.flock(fd, fcntl.LOCK_EX)
+
+    return logger
