f8a48275b22fd6568dfc5a29e1e95f45c0368eee
Belfire98 <93988830+Belfire98@users.noreply.github.com>
2024-03-19 01:23:46 +0100

    improve

--- tests/_passport/test_passportelementerrorfile.py
+++ tests/_passport/test_passportelementerrorfile.py
@@ -1,24 +1,7 @@
 #!/usr/bin/env python
-#
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2024
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
-import pytest
 
-from telegram import PassportElementErrorFile, PassportElementErrorSelfie
+import pytest
+from telegram.errors import PassportElementErrorFile, PassportElementErrorSelfie
 from tests.auxil.slots import mro_slots
 
 
@@ -32,37 +15,39 @@ def passport_element_error_file():
 
 
 class TestPassportElementErrorFileBase:
-    source = "file"
-    type_ = "test_type"
-    file_hash = "file_hash"
-    message = "Error message"
+    source: str = "file"
+    type_: str = "test_type"
+    file_hash: str = "file_hash"
+    message: str = "Error message"
 
 
 class TestPassportElementErrorFileWithoutRequest(TestPassportElementErrorFileBase):
-    def test_slot_behaviour(self, passport_element_error_file):
+    def test_slot_behaviour(self, passport_element_error_file: PassportElementErrorFile) -> None:
+        """Test slot behaviour of PassportElementErrorFile instance."""
         inst = passport_element_error_file
         for attr in inst.__slots__:
             assert getattr(inst, attr, "err") != "err", f"got extra slot '{attr}'"
         assert len(mro_slots(inst)) == len(set(mro_slots(inst))), "duplicate slot"
 
-    def test_expected_values(self, passport_element_error_file):
+    def test_expected_values(self, passport_element_error_file: PassportElementErrorFile) -> None:
+        """Test expected values of PassportElementErrorFile instance."""
         assert passport_element_error_file.source == self.source
         assert passport_element_error_file.type == self.type_
         assert passport_element_error_file.file_hash == self.file_hash
         assert passport_element_error_file.message == self.message
 
-    def test_to_dict(self, passport_element_error_file):
+    def test_to_dict(self, passport_element_error_file: PassportElementErrorFile) -> None:
+        """Test to_dict method of PassportElementErrorFile instance."""
         passport_element_error_file_dict = passport_element_error_file.to_dict()
 
         assert isinstance(passport_element_error_file_dict, dict)
         assert passport_element_error_file_dict["source"] == passport_element_error_file.source
         assert passport_element_error_file_dict["type"] == passport_element_error_file.type
-        assert (
-            passport_element_error_file_dict["file_hash"] == passport_element_error_file.file_hash
-        )
+        assert passport_element_error_file_dict["file_hash"] == passport_element_error_file.file_hash
         assert passport_element_error_file_dict["message"] == passport_element_error_file.message
 
-    def test_equality(self):
+    def test_equality(self) -> None:
+        """Test equality of PassportElementErrorFile instances."""
         a = PassportElementErrorFile(self.type_, self.file_hash, self.message)
         b = PassportElementErrorFile(self.type_, self.file_hash, self.message)
         c = PassportElementErrorFile(self.type_, "", "")
