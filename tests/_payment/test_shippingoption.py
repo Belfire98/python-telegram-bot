06d4f7e4af123c93a6b24ba94d8d8eece950ba08
Belfire98 <93988830+Belfire98@users.noreply.github.com>
2024-03-19 01:23:51 +0100

    improve

--- tests/_payment/test_shippingoption.py
+++ tests/_payment/test_shippingoption.py
@@ -1,53 +1,46 @@
 #!/usr/bin/env python
-#
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2024
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
-import pytest
 
+import pytest
 from telegram import LabeledPrice, ShippingOption, Voice
 from tests.auxil.slots import mro_slots
 
 
-@pytest.fixture(scope="module")
-def shipping_option():
-    return ShippingOption(
-        TestShippingOptionBase.id_, TestShippingOptionBase.title, TestShippingOptionBase.prices
-    )
+@pytest.mark.usefixtures("shipping_option")
+class TestShippingOption:
+    """Tests for the ShippingOption class."""
 
-
-class TestShippingOptionBase:
     id_ = "id"
     title = "title"
     prices = [LabeledPrice("Fish Container", 100), LabeledPrice("Premium Fish Container", 1000)]
 
+    @pytest.mark.parametrize(
+        "test_case_id, id_, title, prices",
+        [
+            ("default", id_, title, prices),
+            ("empty_prices", id_, title, []),
+            ("different_id", "other_id", title, prices),
+            ("different_title", id_, "other_title", prices),
+        ],
+    )
+    def test_expected_values(self, shipping_option, test_case_id, id_, title, prices):
+        """Test that the ShippingOption instance has the expected values."""
+        shipping_option.id = id_
+        shipping_option.title = title
+        shipping_option.prices = prices
 
-class TestShippingOptionWithoutRequest(TestShippingOptionBase):
+        assert shipping_option.id == id_
+        assert shipping_option.title == title
+        assert shipping_option.prices == tuple(prices)
+
     def test_slot_behaviour(self, shipping_option):
+        """Test that the ShippingOption instance behaves correctly with respect to its slots."""
         inst = shipping_option
         for attr in inst.__slots__:
             assert getattr(inst, attr, "err") != "err", f"got extra slot '{attr}'"
         assert len(mro_slots(inst)) == len(set(mro_slots(inst))), "duplicate slot"
 
-    def test_expected_values(self, shipping_option):
-        assert shipping_option.id == self.id_
-        assert shipping_option.title == self.title
-        assert shipping_option.prices == tuple(self.prices)
-
     def test_to_dict(self, shipping_option):
+        """Test that the ShippingOption instance can be serialized to a dictionary."""
         shipping_option_dict = shipping_option.to_dict()
 
         assert isinstance(shipping_option_dict, dict)
@@ -56,22 +49,28 @@ class TestShippingOptionWithoutRequest(TestShippingOptionBase):
         assert shipping_option_dict["prices"][0] == shipping_option.prices[0].to_dict()
         assert shipping_option_dict["prices"][1] == shipping_option.prices[1].to_dict()
 
-    def test_equality(self):
-        a = ShippingOption(self.id_, self.title, self.prices)
-        b = ShippingOption(self.id_, self.title, self.prices)
-        c = ShippingOption(self.id_, "", [])
-        d = ShippingOption(0, self.title, self.prices)
-        e = Voice(self.id_, "someid", 0)
+    @pytest.mark.parametrize(
+        "a_id, b_id, a_title, b_title, a_prices, b_prices, id_func",
+        [
+            (id_, id_, title, title, prices, prices, hash),
+            (id_, id_, title, "", prices, [], hash),
+            (id_, "other_id", title, title, prices, prices, lambda x: 0),
+            (0, id_, title, title, prices, prices, hash),
+            (id_, id_, title, title, prices, [], hash),
+            (id_, id_, title, title, [], prices, hash),
+            (id_, id_, title, title, prices, Voice("someid", "someid", 0).to_dict(), hash),
+        ],
+    )
+    def test_equality(self, shipping_option, a_id, b_id, a_title, b_title, a_prices, b_prices, id_func):
+        """Test that the ShippingOption instance is compared correctly with other instances."""
+        a = ShippingOption(a_id, a_title, a_prices)
+        b = ShippingOption(b_id, b_title, b_prices)
 
-        assert a == b
-        assert hash(a) == hash(b)
-        assert a is not b
+        assert id_func(a) == id_func(b) if a == b else id_func(a) != id_func(b)
 
-        assert a == c
-        assert hash(a) == hash(c)
 
-        assert a != d
-        assert hash(a) != hash(d)
-
-        assert a != e
-        assert hash(a) != hash(e)
+@pytest.fixture(scope="module")
+def shipping_option():
+    return ShippingOption(
+        TestShippingOption.id_, TestShippingOption.title, TestShippingOption.prices
+   
