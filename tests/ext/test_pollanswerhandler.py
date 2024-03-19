f9f6477dda56ede375c536aaa7bea2096a075bb6
Belfire98 <93988830+Belfire98@users.noreply.github.com>
2024-03-19 01:23:50 +0100

    improve

--- tests/ext/test_pollanswerhandler.py
+++ tests/ext/test_pollanswerhandler.py
@@ -1,52 +1,25 @@
 #!/usr/bin/env python
-#
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2024
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
+
 import asyncio
-
 import pytest
+from typing import Any, Callable, Dict, List, Optional
 
-from telegram import (
-    Bot,
-    CallbackQuery,
-    Chat,
-    ChosenInlineResult,
-    Message,
-    PollAnswer,
-    PreCheckoutQuery,
-    ShippingQuery,
-    Update,
-    User,
-)
+import telegram
 from telegram.ext import CallbackContext, JobQueue, PollAnswerHandler
 from tests.auxil.slots import mro_slots
 
-message = Message(1, None, Chat(1, ""), from_user=User(1, "", False), text="Text")
+message = telegram.Message(1, None, telegram.Chat(1, ""), from_user=telegram.User(1, "", False), text="Text")
 
 params = [
     {"message": message},
     {"edited_message": message},
-    {"callback_query": CallbackQuery(1, User(1, "", False), "chat", message=message)},
+    {"callback_query": telegram.CallbackQuery(1, telegram.User(1, "", False), "chat", message=message)},
     {"channel_post": message},
     {"edited_channel_post": message},
-    {"chosen_inline_result": ChosenInlineResult("id", User(1, "", False), "")},
-    {"shipping_query": ShippingQuery("id", User(1, "", False), "", None)},
-    {"pre_checkout_query": PreCheckoutQuery("id", User(1, "", False), "", 0, "")},
-    {"callback_query": CallbackQuery(1, User(1, "", False), "chat")},
+    {"chosen_inline_result": telegram.ChosenInlineResult("id", telegram.User(1, "", False), "")},
+    {"shipping_query": telegram.ShippingQuery("id", telegram.User(1, "", False), "", None)},
+    {"pre_checkout_query": telegram.PreCheckoutQuery("id", telegram.User(1, "", False), "", 0, "")},
+    {"callback_query": telegram.CallbackQuery(1, telegram.User(1, "", False), "chat")},
 ]
 
 ids = (
@@ -64,19 +37,21 @@ ids = (
 
 @pytest.fixture(scope="class", params=params, ids=ids)
 def false_update(request):
-    return Update(update_id=2, **request.param)
+    return telegram.Update(update_id=2, **request.param)
 
 
 @pytest.fixture()
-def poll_answer(bot):
-    return Update(0, poll_answer=PollAnswer(1, [0, 1], User(2, "test user", False), Chat(1, "")))
+def poll_answer() -> telegram.Update:
+    return telegram.Update(
+        0,
+        poll_answer=telegram.PollAnswer(1, [0, 1], telegram.User(2, "test user", False), telegram.Chat(1, "")),
+    )
 
 
 class TestPollAnswerHandler:
-    test_flag = False
+    test_flag: bool
 
-    def test_slot_behaviour(self):
-        handler = PollAnswerHandler(self.callback)
+    def test_slot_behaviour(self, handler: PollAnswerHandler):
         for attr in handler.__slots__:
             assert getattr(handler, attr, "err") != "err", f"got extra slot '{attr}'"
         assert len(mro_slots(handler)) == len(set(mro_slots(handler))), "duplicate slot"
@@ -85,24 +60,24 @@ class TestPollAnswerHandler:
     def _reset(self):
         self.test_flag = False
 
-    async def callback(self, update, context):
+    async def callback(self, update: telegram.Update, context: CallbackContext):
         self.test_flag = (
             isinstance(context, CallbackContext)
-            and isinstance(context.bot, Bot)
-            and isinstance(update, Update)
+            and isinstance(context.bot, telegram.Bot)
+            and isinstance(update, telegram.Update)
             and isinstance(context.update_queue, asyncio.Queue)
             and isinstance(context.job_queue, JobQueue)
             and isinstance(context.user_data, dict)
             and context.chat_data is None
             and isinstance(context.bot_data, dict)
-            and isinstance(update.poll_answer, PollAnswer)
+            and isinstance(update.poll_answer, telegram.PollAnswer)
         )
 
-    def test_other_update_types(self, false_update):
+    def test_other_update_types(self, false_update: telegram.Update):
         handler = PollAnswerHandler(self.callback)
         assert not handler.check_update(false_update)
 
-    async def test_context(self, app, poll_answer):
+    async def test_context(self, app, poll_answer: telegram.Update):
         handler = PollAnswerHandler(self.callback)
         app.add_handler(handler)
 
