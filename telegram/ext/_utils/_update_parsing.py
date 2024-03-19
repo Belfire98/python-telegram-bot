1d9f520076ca52a81163b54af6e4c9286616ec98
Belfire98 <93988830+Belfire98@users.noreply.github.com>
2024-03-19 01:23:51 +0100

    improve

--- telegram/ext/_utils/_update_parsing.py
+++ telegram/ext/_utils/_update_parsing.py
@@ -1,50 +1,42 @@
 #!/usr/bin/env python
-#
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2024
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
-"""This module contains helper functions related to parsing updates and their contents.
 
-.. versionadded:: 20.8
-
-Warning:
-    Contents of this module are intended to be used internally by the library and *not* by the
-    user. Changes to this module are not considered breaking changes and may not be documented in
-    the changelog.
-"""
-from typing import FrozenSet, Optional
+from typing import FrozenSet, Optional, Union
 
 from telegram._utils.types import SCT
 
+def parse_chat_or_username(
+    value: Optional[SCT[Union[int, str]]],
+) -> FrozenSet[Union[int, str]]:
+    """Accepts a chat ID, username, or collection of chat IDs and usernames
+    and returns a frozenset of unique chat IDs and usernames.
 
-def parse_chat_id(chat_id: Optional[SCT[int]]) -> FrozenSet[int]:
-    """Accepts a chat id or collection of chat ids and returns a frozenset of chat ids."""
-    if chat_id is None:
-        return frozenset()
-    if isinstance(chat_id, int):
-        return frozenset({chat_id})
-    return frozenset(chat_id)
+    Strips the leading '@' from usernames if present.
 
+    Args:
+        value (Optional[SCT[Union[int, str]]]): A chat ID, username, or collection
+            of chat IDs and usernames.
 
-def parse_username(username: Optional[SCT[str]]) -> FrozenSet[str]:
-    """Accepts a username or collection of usernames and returns a frozenset of usernames.
-    Strips the leading ``@`` if present.
+    Returns:
+        FrozenSet[Union[int, str]]: A frozenset of unique chat IDs and usernames.
+
+    Raises:
+        ValueError: If the input value is invalid.
     """
-    if username is None:
+    if value is None:
         return frozenset()
-    if isinstance(username, str):
-        return frozenset({username[1:] if username.startswith("@") else username})
-    return frozenset({usr[1:] if usr.startswith("@") else usr for usr in username})
+
+    result = set()
+    for item in value:
+        if isinstance(item, int):
+            result.add(item)
+        elif isinstance(item, str):
+            if item.startswith("@"):
+                item = item[1:]
+            if item.isdigit():
+                result.add(int(item))
+            else:
+                result.add(item)
+        else:
+            raise ValueError(f"Invalid value: {item}")
+
+    return frozenset(result)
