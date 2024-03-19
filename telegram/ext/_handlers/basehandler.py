e19908cdcc34df429feda22ae9cc201d0faceb6b
Belfire98 <93988830+Belfire98@users.noreply.github.com>
2024-03-19 01:23:47 +0100

    improve

--- telegram/ext/_handlers/basehandler.py
+++ telegram/ext/_handlers/basehandler.py
@@ -1,25 +1,9 @@
 #!/usr/bin/env python
-#
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2024
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
-"""This module contains the base class for handlers as used by the Application."""
-from abc import ABC, abstractmethod
-from typing import TYPE_CHECKING, Any, Generic, Optional, TypeVar, Union
 
+from abc import ABC, abstractmethod
+from typing import Any, Dict, Generic, Optional, Type, TypeVar, Union
+
+import telegram
 from telegram._utils.defaultvalue import DEFAULT_TRUE
 from telegram._utils.repr import build_repr_with_selected_attrs
 from telegram._utils.types import DVType
@@ -61,21 +45,6 @@ class BaseHandler(Generic[UT, CCT], ABC):
 
         * The attribute ``run_async`` is now :paramref:`block`.
         * This class was previously named ``Handler``.
-
-    Args:
-        callback (:term:`coroutine function`): The callback function for this handler. Will be
-            called when :meth:`check_update` has determined that an update should be processed by
-            this handler. Callback signature::
-
-                async def callback(update: Update, context: CallbackContext)
-
-            The return value of the callback is usually ignored except for the special case of
-            :class:`telegram.ext.ConversationHandler`.
-        block (:obj:`bool`, optional): Determines whether the return value of the callback should
-            be awaited before processing the next handler in
-            :meth:`telegram.ext.Application.process_update`. Defaults to :obj:`True`.
-
-            .. seealso:: :wiki:`Concurrency`
 
     Attributes:
         callback (:term:`coroutine function`): The callback function for this handler.
@@ -134,41 +103,4 @@ class BaseHandler(Generic[UT, CCT], ABC):
     async def handle_update(
         self,
         update: UT,
-        application: "Application[Any, CCT, Any, Any, Any, Any]",
-        check_result: object,
-        context: CCT,
-    ) -> RT:
-        """
-        This method is called if it was determined that an update should indeed
-        be handled by this instance. Calls :attr:`callback` along with its respectful
-        arguments. To work with the :class:`telegram.ext.ConversationHandler`, this method
-        returns the value returned from :attr:`callback`.
-        Note that it can be overridden if needed by the subclassing handler.
-
-        Args:
-            update (:obj:`str` | :class:`telegram.Update`): The update to be handled.
-            application (:class:`telegram.ext.Application`): The calling application.
-            check_result (:class:`object`): The result from :meth:`check_update`.
-            context (:class:`telegram.ext.CallbackContext`): The context as provided by
-                the application.
-
-        """
-        self.collect_additional_context(context, update, application, check_result)
-        return await self.callback(update, context)
-
-    def collect_additional_context(
-        self,
-        context: CCT,
-        update: UT,
-        application: "Application[Any, CCT, Any, Any, Any, Any]",
-        check_result: Any,
-    ) -> None:
-        """Prepares additional arguments for the context. Override if needed.
-
-        Args:
-            context (:class:`telegram.ext.CallbackContext`): The context object.
-            update (:class:`telegram.Update`): The update to gather chat/user id from.
-            application (:class:`telegram.ext.Application`): The calling application.
-            check_result: The result (return value) from :meth:`check_update`.
-
-        """
+        application: "Application[Any, CCT, Any, Any, Any, Any
