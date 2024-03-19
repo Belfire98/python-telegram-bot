33c8a830d526c69a811f80f2d755ab467fad1337
Belfire98 <93988830+Belfire98@users.noreply.github.com>
2024-03-19 01:23:54 +0100

    improve

--- tests/_inline/test_inlinequeryresultmpeg4gif.py
+++ tests/_inline/test_inlinequeryresultmpeg4gif.py
@@ -1,23 +1,6 @@
 #!/usr/bin/env python
-#
-# A library that provides a Python interface to the Telegram Bot API
-# Copyright (C) 2015-2024
-# Leandro Toledo de Souza <devs@python-telegram-bot.org>
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser Public License for more details.
-#
-# You should have received a copy of the GNU Lesser Public License
-# along with this program.  If not, see [http://www.gnu.org/licenses/].
-import pytest
 
+import pytest
 from telegram import (
     InlineKeyboardButton,
     InlineKeyboardMarkup,
@@ -28,51 +11,48 @@ from telegram import (
 )
 from tests.auxil.slots import mro_slots
 
-
-@pytest.fixture(scope="module")
-def inline_query_result_mpeg4_gif():
-    return InlineQueryResultMpeg4Gif(
-        TestInlineQueryResultMpeg4GifBase.id_,
-        TestInlineQueryResultMpeg4GifBase.mpeg4_url,
-        TestInlineQueryResultMpeg4GifBase.thumbnail_url,
-        mpeg4_width=TestInlineQueryResultMpeg4GifBase.mpeg4_width,
-        mpeg4_height=TestInlineQueryResultMpeg4GifBase.mpeg4_height,
-        mpeg4_duration=TestInlineQueryResultMpeg4GifBase.mpeg4_duration,
-        title=TestInlineQueryResultMpeg4GifBase.title,
-        caption=TestInlineQueryResultMpeg4GifBase.caption,
-        parse_mode=TestInlineQueryResultMpeg4GifBase.parse_mode,
-        caption_entities=TestInlineQueryResultMpeg4GifBase.caption_entities,
-        input_message_content=TestInlineQueryResultMpeg4GifBase.input_message_content,
-        reply_markup=TestInlineQueryResultMpeg4GifBase.reply_markup,
-        thumbnail_mime_type=TestInlineQueryResultMpeg4GifBase.thumbnail_mime_type,
-    )
-
-
 class TestInlineQueryResultMpeg4GifBase:
-    id_ = "id"
-    type_ = "mpeg4_gif"
-    mpeg4_url = "mpeg4 url"
-    mpeg4_width = 10
-    mpeg4_height = 15
-    mpeg4_duration = 1
-    thumbnail_url = "thumb url"
-    thumbnail_mime_type = "image/jpeg"
-    title = "title"
-    caption = "caption"
-    parse_mode = "Markdown"
-    caption_entities = [MessageEntity(MessageEntity.ITALIC, 0, 7)]
-    input_message_content = InputTextMessageContent("input_message_content")
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("reply_markup")]])
+    id_: str = "id"
+    type_: str = "mpeg4_gif"
+    mpeg4_url: str = "mpeg4 url"
+    mpeg4_width: int = 10
+    mpeg4_height: int = 15
+    mpeg4_duration: int = 1
+    thumbnail_url: str = "thumb url"
+    thumbnail_mime_type: str = "image/jpeg"
+    title: str = "title"
+    caption: str = "caption"
+    parse_mode: str = "Markdown"
+    caption_entities: tuple[MessageEntity] = tuple([MessageEntity(MessageEntity.ITALIC, 0, 7)])
+    input_message_content: InputTextMessageContent = InputTextMessageContent("input_message_content")
+    reply_markup: InlineKeyboardMarkup = InlineKeyboardMarkup([[InlineKeyboardButton("reply_markup")]])
 
+class TestInlineQueryResultMpeg4Gif(TestInlineQueryResultMpeg4GifBase):
+    @pytest.fixture(scope="module")
+    def inline_query_result_mpeg4_gif(self):
+        return InlineQueryResultMpeg4Gif(
+            self.id_,
+            self.mpeg4_url,
+            self.thumbnail_url,
+            mpeg4_width=self.mpeg4_width,
+            mpeg4_height=self.mpeg4_height,
+            mpeg4_duration=self.mpeg4_duration,
+            title=self.title,
+            caption=self.caption,
+            parse_mode=self.parse_mode,
+            caption_entities=self.caption_entities,
+            input_message_content=self.input_message_content,
+            reply_markup=self.reply_markup,
+            thumbnail_mime_type=self.thumbnail_mime_type,
+        )
 
-class TestInlineQueryResultMpeg4GifWithoutRequest(TestInlineQueryResultMpeg4GifBase):
-    def test_slot_behaviour(self, inline_query_result_mpeg4_gif):
+    def test_slot_behaviour(self, inline_query_result_mpeg4_gif: InlineQueryResultMpeg4Gif):
         inst = inline_query_result_mpeg4_gif
         for attr in inst.__slots__:
             assert getattr(inst, attr, "err") != "err", f"got extra slot '{attr}'"
         assert len(mro_slots(inst)) == len(set(mro_slots(inst))), "duplicate slot"
 
-    def test_expected_values(self, inline_query_result_mpeg4_gif):
+    def test_expected_values(self, inline_query_result_mpeg4_gif: InlineQueryResultMpeg4Gif):
         assert inline_query_result_mpeg4_gif.type == self.type_
         assert inline_query_result_mpeg4_gif.id == self.id_
         assert inline_query_result_mpeg4_gif.mpeg4_url == self.mpeg4_url
@@ -84,7 +64,7 @@ class TestInlineQueryResultMpeg4GifWithoutRequest(TestInlineQueryResultMpeg4GifB
         assert inline_query_result_mpeg4_gif.title == self.title
         assert inline_query_result_mpeg4_gif.caption == self.caption
         assert inline_query_result_mpeg4_gif.parse_mode == self.parse_mode
-        assert inline_query_result_mpeg4_gif.caption_entities == tuple(self.caption_entities)
+        assert inline_query_result_mpeg4_gif.caption_entities == self.caption_entities
         assert (
             inline_query_result_mpeg4_gif.input_message_content.to_dict()
             == self.input_message_content.to_dict()
@@ -95,7 +75,7 @@ class TestInlineQueryResultMpeg4GifWithoutRequest(TestInlineQueryResultMpeg4GifB
         result = InlineQueryResultMpeg4Gif(self.id_, self.mpeg4_url, self.thumbnail_url)
         assert result.caption_entities == ()
 
-    def test_to_dict(self, inline_query_result_mpeg4_gif):
+    def test_to_dict(self, inline_query_result_mpeg4_gif: InlineQueryResultMpeg4Gif):
         inline_query_result_mpeg4_gif_dict = inline_query_result_mpeg4_gif.to_dict()
 
         assert isinstance(inline_query_result_mpeg4_gif_dict, dict)
@@ -106,61 +86,4 @@ class TestInlineQueryResultMpeg4GifWithoutRequest(TestInlineQueryResultMpeg4GifB
             == inline_query_result_mpeg4_gif.mpeg4_url
         )
         assert (
-            inline_query_result_mpeg4_gif_dict["mpeg4_width"]
-            == inline_query_result_mpeg4_gif.mpeg4_width
-        )
-        assert (
-            inline_query_result_mpeg4_gif_dict["mpeg4_height"]
-            == inline_query_result_mpeg4_gif.mpeg4_height
-        )
-        assert (
-            inline_query_result_mpeg4_gif_dict["mpeg4_duration"]
-            == inline_query_result_mpeg4_gif.mpeg4_duration
-        )
-        assert (
-            inline_query_result_mpeg4_gif_dict["thumbnail_url"]
-            == inline_query_result_mpeg4_gif.thumbnail_url
-        )
-        assert (
-            inline_query_result_mpeg4_gif_dict["thumbnail_mime_type"]
-            == inline_query_result_mpeg4_gif.thumbnail_mime_type
-        )
-        assert inline_query_result_mpeg4_gif_dict["title"] == inline_query_result_mpeg4_gif.title
-        assert (
-            inline_query_result_mpeg4_gif_dict["caption"] == inline_query_result_mpeg4_gif.caption
-        )
-        assert (
-            inline_query_result_mpeg4_gif_dict["parse_mode"]
-            == inline_query_result_mpeg4_gif.parse_mode
-        )
-        assert inline_query_result_mpeg4_gif_dict["caption_entities"] == [
-            ce.to_dict() for ce in inline_query_result_mpeg4_gif.caption_entities
-        ]
-        assert (
-            inline_query_result_mpeg4_gif_dict["input_message_content"]
-            == inline_query_result_mpeg4_gif.input_message_content.to_dict()
-        )
-        assert (
-            inline_query_result_mpeg4_gif_dict["reply_markup"]
-            == inline_query_result_mpeg4_gif.reply_markup.to_dict()
-        )
-
-    def test_equality(self):
-        a = InlineQueryResultMpeg4Gif(self.id_, self.mpeg4_url, self.thumbnail_url)
-        b = InlineQueryResultMpeg4Gif(self.id_, self.mpeg4_url, self.thumbnail_url)
-        c = InlineQueryResultMpeg4Gif(self.id_, "", self.thumbnail_url)
-        d = InlineQueryResultMpeg4Gif("", self.mpeg4_url, self.thumbnail_url)
-        e = InlineQueryResultVoice(self.id_, "", "")
-
-        assert a == b
-        assert hash(a) == hash(b)
-        assert a is not b
-
-        assert a == c
-        assert hash(a) == hash(c)
-
-        assert a != d
-        assert hash(a) != hash(d)
-
-        assert a != e
-        assert hash(a) != hash(e)
+           
